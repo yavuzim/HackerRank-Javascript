3f9a3a07fe4ea6d71bacb75d3ebea337f5955215
90539 <yavuz.imer133@gmail.com>
2021-09-14 13:14:48 +0300

    Angry-Professor

--- Angry-Professor/problem.js
+++ Angry-Professor/problem.js
@@ -1,42 +1,36 @@
-/*
- * Complete the 'angryProfessor' function below.
- *
- * The function is expected to return a STRING.
- * The function accepts following parameters:
- *  1. INTEGER k
- *  2. INTEGER_ARRAY a
- */
 
-function angryProfessor(k, a) {   
-    let _count=0; 
-    let _status;
-    for(let i=0; i<a.length;i++){
-        if(a[i]<=0) _count++;
+function angryProfessor(k, a) {
+    // Write your code here
+    let count = 0, status
+
+    a.map(e => {
+        if (e <= 0) count++
+    })
+
+    if (count < k) status = "YES"
+    else if (count >= k) status = "NO"
+
+    return status
+
+
+    function main() {
+        const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+        const t = parseInt(readLine().trim(), 10);
+
+        for (let tItr = 0; tItr < t; tItr++) {
+            const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+            const n = parseInt(firstMultipleInput[0], 10);
+
+            const k = parseInt(firstMultipleInput[1], 10);
+
+            const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+            const result = angryProfessor(k, a);
+
+            ws.write(result + '\n');
+        }
+
+        ws.end();
     }
-    if(_count<k) _status="YES"
-    else if(_count>=k) _status="NO"
-    
-    return _status;
-}
-
-function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-    const t = parseInt(readLine().trim(), 10);
-
-    for (let tItr = 0; tItr < t; tItr++) {
-        const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
-
-        const n = parseInt(firstMultipleInput[0], 10);
-
-        const k = parseInt(firstMultipleInput[1], 10);
-
-        const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
-
-        const result = angryProfessor(k, a);
-
-        ws.write(result + '\n');
-    }
-
-    ws.end();
-}
